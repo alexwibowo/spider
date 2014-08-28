a232ff15a9085e634a8a2e9050ddda787bdd756f
Alex Wibowo <alexwibowo@gmail.com>
2014-08-28 22:15:58 +1000

    Redesign UI a little bit. Omit textarea.

--- src/main/java/org/github/alexwibowo/spider/gui/MainPanel.java
+++ src/main/java/org/github/alexwibowo/spider/gui/MainPanel.java
@@ -4,8 +4,11 @@
 
 package org.github.alexwibowo.spider.gui;
 
+import java.awt.*;
+import com.jgoodies.forms.factories.*;
 import com.jgoodies.forms.factories.CC;
 import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.uif_lite.panel.*;
 import org.github.alexwibowo.spider.gui.model.BarcodeMainPanelPresentationModel;
 
 import javax.swing.*;
@@ -18,18 +21,22 @@ public class MainPanel extends BasePanel<BarcodeMainPanelPresentationModel> {
 
 	protected void initComponents() {
 		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+		DefaultComponentFactory compFactory = DefaultComponentFactory.getInstance();
 		menuToolBar = new JToolBar();
 		openFolderButton = new JButton();
 		panel2 = new JPanel();
+		simpleInternalFrame1 = new SimpleInternalFrame();
+		fileTableContainer = new JScrollPane();
+		fileTable = new JTable();
 		controlContainer = new JPanel();
+		settingsLabel = compFactory.createSeparator("Output Settings");
+		referenceFileLabel = new JLabel();
+		referenceFileTextField = new JTextField();
+		controlFileBrowseButton = new JButton();
 		saveToLabel = new JLabel();
 		targetDirectoryTextField = new JTextField();
 		targetDirectoryBrowseButton = new JButton();
-		fileTableContainer = new JScrollPane();
-		fileTable = new JTable();
 		processButton = new JButton();
-		progressContainer = new JScrollPane();
-		progressTextArea = new JTextArea();
 
 		//======== this ========
 		setLayout(new FormLayout(
@@ -51,43 +58,54 @@ public class MainPanel extends BasePanel<BarcodeMainPanelPresentationModel> {
 		{
 			panel2.setLayout(new FormLayout(
 				"$ugap, default:grow, $ugap",
-				"default, $lgap, fill:default, $lgap, default, $lgap, fill:pref:grow, $lgap, $nlgap"));
+				"fill:default:grow, $lgap, default, $lgap, $nlgap"));
+
+			//======== simpleInternalFrame1 ========
+			{
+				simpleInternalFrame1.setTitle("Directory Contents");
+				Container simpleInternalFrame1ContentPane = simpleInternalFrame1.getContentPane();
+				simpleInternalFrame1ContentPane.setLayout(new FormLayout(
+					"default:grow",
+					"fill:default:grow"));
+
+				//======== fileTableContainer ========
+				{
+					fileTableContainer.setViewportView(fileTable);
+				}
+				simpleInternalFrame1ContentPane.add(fileTableContainer, CC.xy(1, 1));
+			}
+			panel2.add(simpleInternalFrame1, CC.xy(2, 1));
 
 			//======== controlContainer ========
 			{
 				controlContainer.setLayout(new FormLayout(
-					"default, $lcgap, default:grow, $lcgap, default",
-					"default"));
+					"right:default, $lcgap, default:grow, $lcgap, default",
+					"3*(default, $lgap), default"));
+				controlContainer.add(settingsLabel, CC.xywh(1, 1, 5, 1));
+
+				//---- referenceFileLabel ----
+				referenceFileLabel.setText("Reference File");
+				controlContainer.add(referenceFileLabel, CC.xy(1, 3));
+				controlContainer.add(referenceFileTextField, CC.xy(3, 3));
+
+				//---- controlFileBrowseButton ----
+				controlFileBrowseButton.setText("Browse");
+				controlContainer.add(controlFileBrowseButton, CC.xy(5, 3));
 
 				//---- saveToLabel ----
 				saveToLabel.setText("Save to");
-				controlContainer.add(saveToLabel, CC.xy(1, 1));
-				controlContainer.add(targetDirectoryTextField, CC.xy(3, 1));
+				controlContainer.add(saveToLabel, CC.xy(1, 5));
+				controlContainer.add(targetDirectoryTextField, CC.xy(3, 5));
 
 				//---- targetDirectoryBrowseButton ----
 				targetDirectoryBrowseButton.setText("Browse");
-				controlContainer.add(targetDirectoryBrowseButton, CC.xy(5, 1));
+				controlContainer.add(targetDirectoryBrowseButton, CC.xy(5, 5));
+
+				//---- processButton ----
+				processButton.setText("Process");
+				controlContainer.add(processButton, CC.xy(3, 7, CC.LEFT, CC.DEFAULT));
 			}
-			panel2.add(controlContainer, CC.xy(2, 1, CC.FILL, CC.DEFAULT));
-
-			//======== fileTableContainer ========
-			{
-				fileTableContainer.setViewportView(fileTable);
-			}
-			panel2.add(fileTableContainer, CC.xy(2, 3, CC.FILL, CC.FILL));
-
-			//---- processButton ----
-			processButton.setText("Process");
-			panel2.add(processButton, CC.xy(2, 5, CC.LEFT, CC.DEFAULT));
-
-			//======== progressContainer ========
-			{
-
-				//---- progressTextArea ----
-				progressTextArea.setRows(10);
-				progressContainer.setViewportView(progressTextArea);
-			}
-			panel2.add(progressContainer, CC.xy(2, 7, CC.FILL, CC.FILL));
+			panel2.add(controlContainer, CC.xy(2, 3, CC.FILL, CC.DEFAULT));
 		}
 		add(panel2, CC.xy(1, 3, CC.FILL, CC.FILL));
 		// JFormDesigner - End of component initialization  //GEN-END:initComponents
@@ -97,14 +115,17 @@ public class MainPanel extends BasePanel<BarcodeMainPanelPresentationModel> {
 	protected JToolBar menuToolBar;
 	protected JButton openFolderButton;
 	protected JPanel panel2;
+	protected SimpleInternalFrame simpleInternalFrame1;
+	protected JScrollPane fileTableContainer;
+	protected JTable fileTable;
 	protected JPanel controlContainer;
+	protected JComponent settingsLabel;
+	protected JLabel referenceFileLabel;
+	protected JTextField referenceFileTextField;
+	protected JButton controlFileBrowseButton;
 	protected JLabel saveToLabel;
 	protected JTextField targetDirectoryTextField;
 	protected JButton targetDirectoryBrowseButton;
-	protected JScrollPane fileTableContainer;
-	protected JTable fileTable;
 	protected JButton processButton;
-	protected JScrollPane progressContainer;
-	protected JTextArea progressTextArea;
 	// JFormDesigner - End of variables declaration  //GEN-END:variables
 }
